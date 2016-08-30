4c4c1fce7a35a9a89ce039241e9009959a8f6cef
ThrDev <zac@stykocompany.com>
2016-08-30 02:06:00 +0000

    Second push, adding more documentation & code patches.

---
+++ qmvc/views/IndexController/index.view.php
@@ -0,0 +1,11 @@
+<h3>Welcome to qmvc!</h3>
+
+<p>This is a dynamic variable name: <?php echo $title; ?>.</p>
+
+<p>This page was generated with a layout, a view, and an element.</p>
+
+<p>This content you see here is the view, titled <?php echo $this->request->path["view"]; ?>.view.php.</p>
+
+<p>The layout file controls the outer HTML portion.</p>
+
+<p id="element"></p>
---
+++ qmvc/views/elements/head.element.php
@@ -0,0 +1,6 @@
+<script>
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var p = document.getElementById("element");
+    p.innerHTML = "The head element is loaded in and wrote this with javascript.";
+});
+</script>
