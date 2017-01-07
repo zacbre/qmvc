829c3c1066c4eb42dd3988923ed783e021d5ee6e
ThrDev <zac@stykocompany.com>
2017-01-07 02:20:09 +0000

    Changed version to 0.9.7a

--- qmvc/app/init.inc.php
+++ qmvc/app/init.inc.php
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
     
-    @version 0.9.1a
+    @version 0.9.7a
 */
 
 ob_start();
